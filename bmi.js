bdef5869a6e780f3d22294431a469ff2d4b09cdd
Taylor Sargent <tesarge@gmail.com>
2022-11-10 17:05:26 +0800

    Update bmi.js
    Updated bmi.js to use Tailwind CSS classes on output text

--- bmi.js
+++ bmi.js
@@ -13,15 +13,14 @@ calculate.addEventListener("click", function(e){
     // Use toFixed(1) to round to the nearest tenth
     bmi = Number(bmi).toFixed(1);
     if(bmi < 18.5){
-        document.querySelector("#result").innerHTML = `<div id="uwob"><h1>${bmi}</h1> <p>You are underweight</p></div>`;    
+        document.querySelector("#result").innerHTML = `<div class="text-yellow-500"><h1>${bmi}</h1> <p>You are underweight</p></div>`;    
     } else if(bmi >= 18.5 && bmi < 25){
-        document.querySelector("#result").innerHTML = `<div id="nrml"><h1>${bmi}</h1> <p>Your weight is normal</p></div>`;
+        document.querySelector("#result").innerHTML = `<div class="text-emerald-500"><h1>${bmi}</h1> <p>Your weight is normal</p></div>`;
     } else if(bmi >= 25 && bmi < 30) {
-        document.querySelector("#result").innerHTML = `<div id="ow"><h1>${bmi}</h1> <p>You are overweight</p></div>`;
+        document.querySelector("#result").innerHTML = `<div class="text-orange-500"><h1>${bmi}</h1> <p>You are overweight</p></div>`;
     } else {
-        document.querySelector("#result").innerHTML = `<div id="uwob"><h1>${bmi}</h1> <p>You are obese</p></div>`;
+        document.querySelector("#result").innerHTML = `<div class="text-red-500"><h1>${bmi}</h1> <p>You are obese</p></div>`;
     }
 })
-
 
 
